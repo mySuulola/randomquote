6e8b15faaa28f934dae169ee2338b6aae5410e4d
mySuulola <oluwaseyisuulola@gmail.com>
2019-01-15 19:28:34 +0100

    Updates

---
+++ precache-manifest.ddb015e97bd3fc74ab09f057ed7f753f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "539267bcd3a0a4e0f660",
+    "url": "/randomquote/static/js/runtime~main.539267bc.js"
+  },
+  {
+    "revision": "def89aa9daa6a4bf4093",
+    "url": "/randomquote/static/js/main.def89aa9.chunk.js"
+  },
+  {
+    "revision": "f715b2e0bea123466b14",
+    "url": "/randomquote/static/js/1.f715b2e0.chunk.js"
+  },
+  {
+    "revision": "def89aa9daa6a4bf4093",
+    "url": "/randomquote/static/css/main.a45f448e.chunk.css"
+  },
+  {
+    "revision": "3da54d34fb304bde9e64f9b71ff25088",
+    "url": "/randomquote/index.html"
+  }
+];
